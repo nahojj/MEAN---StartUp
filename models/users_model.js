c59247c0ccc0631ba1718ac6475ef6efc1494ec3
anderskristo <anderskristo@gmail.com>
2015-04-11 21:30:04 +0200

    added more types to model

--- models/users_model.js
+++ models/users_model.js
@@ -2,10 +2,30 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: { type: String, unique: true },
-    email: String,
-    color: String,
-    hashed_password: String
+    username: {
+      type: String,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      trim: true,
+		  default: '',
+      match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    color: {
+      type: String,
+  		trim: true,
+  		default: ''
+    },
+    hashed_password: {
+      type: String,
+  		default: ''
+    },
+    created: {
+  		type: Date,
+  		default: Date.now
+	  }
 });
 
 mongoose.model('User', UserSchema);
